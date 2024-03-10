394b07e400b9ebc94028ada0101ec6ba42aad5b3
Pradeu <medovoi.dmitry@yandex.ru>
2024-03-11 00:13:05 +0300

    Initial commit

---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <NuxtLayout/>
+    <div class="flex justify-center w-15 font-sans hover:font-bold">
+        Мой проект
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <NuxtLayout/>
+    <div class="flex justify-center w-15 font-sans hover:font-bold">Главная страница</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
